83e38dd07330b7b920d8a57b59fc49eab3f44b93
angel_in_a_jar <ruslan_tyu@mail.ru>
2023-10-25 19:51:59 +0300

    refactor `books.py`

--- data/books.py
+++ data/books.py
@@ -1,18 +1,19 @@
-import sqlalchemy
+from sqlalchemy import String, Column, Integer
 
-from .db_session import SqlAlchemyBase
+from db_session import SqlAlchemyBase
 
 
 class Books(SqlAlchemyBase):
     __tablename__ = 'books'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    login = sqlalchemy.Column(sqlalchemy.String)
-    title = sqlalchemy.Column(sqlalchemy.String)
-    authors = sqlalchemy.Column(sqlalchemy.String)
-    date = sqlalchemy.Column(sqlalchemy.String)
-    image = sqlalchemy.Column(sqlalchemy.String)
-    description = sqlalchemy.Column(sqlalchemy.String)
-    language = sqlalchemy.Column(sqlalchemy.String)
-    count = sqlalchemy.Column(sqlalchemy.String)
-    href = sqlalchemy.Column(sqlalchemy.String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    login = Column(String(50))
+    title = Column(String(255))
+    authors = Column(String(255))
+    date = Column(String(255))
+    image = Column(String(255))
+    description = Column(String(255))
+    language = Column(String(255))
+    # TODO: count = Column(String) ???
+    count = Column(Integer)
+    href = Column(String(255))
