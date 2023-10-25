7fe185d873b95fd3948bf8655a39b7012b21b622
angel_in_a_jar <ruslan_tyu@mail.ru>
2023-10-25 23:42:25 +0300

    minor changes in `db_session.py`

--- data/db_session.py
+++ data/db_session.py
@@ -7,7 +7,14 @@ SqlAlchemyBase = declarative_base()
 __factory = None
 
 
-def global_init(db_file):
+def global_init(db_file: str) -> None:
+    """
+    Initialize the database connection.
+
+    :param db_file: Path to the database file.
+    :type db_file: str
+    :raises Exception: No database name or error initializing database.
+    """
     global __factory
 
     if __factory:
@@ -16,15 +23,26 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception('Database file must be specified.')
 
+    # TODO: make it in config
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+
     print(f'Connecting to database: {conn_str}')
 
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = sessionmaker(bind=engine)
+    try:
+        engine = sa.create_engine(conn_str, echo=False)
+        __factory = sessionmaker(bind=engine)
 
-    SqlAlchemyBase.metadata.create_all(engine)
+        SqlAlchemyBase.metadata.create_all(engine)
+    except Exception as e:
+        raise Exception(f'Error initializing database: {e}')
 
 
 def create_session() -> Session:
+    """
+    Create a new session.
+
+    :return: A new SQLAlchemy session.
+    :rtype: Session
+    """
     global __factory
     return __factory()
